45b2c8fa3ff0a7483462ac9f79cc521b32d10ae1
Morgan <55892175+morganpothoff@users.noreply.github.com>
2021-11-13 20:56:29 -0600

    Updates DB for Requests to have an Event.id

--- App/DB_Connections.py
+++ App/DB_Connections.py
@@ -29,7 +29,7 @@ def DB_CONNECTION():
 
 
 def INSERT_request(email: str, reason: str, time: datetime, EventTypes_id: int, is_virtual: bool, zip: int) -> bool:
-	query = """
+	query =	"""
 			INSERT INTO `Requests` (`email`, `reason`, `time`, `EventTypes_id`, `is_virtual`, `zip`) VALUES
 			(%s, %s, %s, %s, %s, %s);
 			"""
@@ -38,5 +38,64 @@ def INSERT_request(email: str, reason: str, time: datetime, EventTypes_id: int, 
 	cursor.execute(query, email, reason, time, EventTypes_id, is_virtual, zip);
 	cnx.commit();
 	return cursor.lastrowid;
+
+
+def INSERT_events(reason: str, EventType_id: int, address: str, state: str, zip: str, time: datetime) -> bool:
+	query =	"""
+			INSERT INTO `Events` (`reason`, `EventType_id`, `address`, `state`, `zip`, `time`) VALUES
+			(%s, %s, %s, %s, %s, %s);
+			"""
+
+	cnx, cursor = DB_CONNECTION()
+	cursor.execute(reason, EventType_id, address, state, zip, time);
+	cnx.commit();
+	return cursor.lastrowid;
+	
+
+def SELECT_requests_by_Event_id(Events_id: int) -> list:
+	query =	"""
+			SELECT * FROM `Requests` WHERE `Events.id` = %s;
+			"""
+
+	cnx, cursor = DB_CONNECTION()
+	cursor.execute(query, Events_id);
+	columns = [column[0] for column in cursor._description]
+	return [{column: (row[x] if row[x] else None) for x, column in enumerate(headers)} for row in cursor._rows]
+
+
+def SELECT_event(Events_id: int) -> dict:
+	query =	"""
+			SELECT * FROM `Requests` WHERE `id` = %s;
+			"""
+
+	cnx, cursor = DB_CONNECTION()
+	cursor.execute(query, Events_id);
+	columns = [column[0] for column in cursor._description]
+	return [{column: (row[x] if row[x] else None) for x, column in enumerate(headers)} for row in cursor._rows]
+
+
+def SELECT_events_by_zip(zip: str) -> dict:
+	query =	"""
+			SELECT * FROM `Requests` WHERE `zip` = %s;
+			"""
+
+	cnx, cursor = DB_CONNECTION()
+	cursor.execute(query, zip);
+	columns = [column[0] for column in cursor._description]
+	return [{column: (row[x] if row[x] else None) for x, column in enumerate(headers)} for row in cursor._rows]
+
+
+def SELECT_Counselors_by_Events_id(Events_id: int) -> dict:
+	query =	"""
+			SELECT * FROM `Counselors`
+			JOIN `EventsCounselors` ON `Counselors`.`Counselors.id` = `EventsCounselors`.`Events.id`
+			WHERE `EventsCounselors`.`Events.id` = %s;
+			"""
+
+	cnx, cursor = DB_CONNECTION()
+	cursor.execute(query, Events_id);
+	columns = [column[0] for column in cursor._description]
+	return [{column: (row[x] if row[x] else None) for x, column in enumerate(headers)} for row in cursor._rows]
+
 
 
